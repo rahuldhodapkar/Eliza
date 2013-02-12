29915149f28dbc6bb83ffc40ef76e426641ca1bc
Rahul Dhodapkar <rahul.dhodapkar@vantagelocal.com>
2013-02-11 21:29:58 -0500

    writing initial file for commit

---
+++ Eliza.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// this is a project to emulate the emergence of inflection patterns that lead to language.
+// we will use connections and connection restraints in a time-based way to construct
+// our model.
+//
+// we will then use this to push our information to a sound-synthesizer that works by
+// replicating the *kinds* of sounds that a human can make, and then use a training
+// program to determine what is good and what is not.  Once we have developed a kind
+// of basis our program should be able to train itself spontaneously and learn new things
+// simply from having conversations with other people.  That would be the greatest basis
+// for intelligence.
+//
+// Rahul Dhodapkar
+// 2.11.13
+
+
+
+
+
+
